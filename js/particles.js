f853a8ad2a34abef153f5e27f6021e4e954f1d69
Cathy Wu <xinai.cathy.wu@gmail.com>
2019-09-21 23:50:03 -0400

    v2

--- js/particles.js
+++ js/particles.js
@@ -1,34 +1,42 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 6, density: { enable: true, value_area: 800 } },
-    color: { value: "#0c5761" },
+    number: {
+      value: 10,
+      density: { enable: true, value_area: 1362.4196573113848 }
+    },
+    color: { value: "#71aca9" },
     shape: {
       type: "polygon",
-      stroke: { width: 0, color: "#000" },
+      stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 6 },
       image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.3,
+      value: 0.5,
       random: true,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      anim: { enable: false, speed: 1, opacity_min: 0.3, sync: false }
     },
     size: {
-      value: 112.16780303333778,
-      random: false,
-      anim: { enable: true, speed: 10, size_min: 40, sync: false }
+      value: 104.18503261792942,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 4.870367063331008,
+        size_min: 100,
+        sync: false
+      }
     },
     line_linked: {
       enable: false,
-      distance: 200,
+      distance: 500,
       color: "#ffffff",
-      opacity: 1,
+      opacity: 0.4,
       width: 2
     },
     move: {
       enable: true,
       speed: 5,
-      direction: "none",
+      direction: "top-right",
       random: false,
       straight: false,
       out_mode: "out",
@@ -39,13 +47,13 @@ particlesJS("particles-js", {
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: false, mode: "grab" },
-      onclick: { enable: false, mode: "push" },
+      onhover: { enable: false, mode: "bubble" },
+      onclick: { enable: false, mode: "repulse" },
       resize: true
     },
     modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      grab: { distance: 400, line_linked: { opacity: 0.5 } },
+      bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
