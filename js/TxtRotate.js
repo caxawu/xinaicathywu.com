416efb969bf23c63ce0982ea7167a8b2759bbed4
Cathy Wu <xinai.cathy.wu@gmail.com>
2019-12-13 00:08:27 -0500

    Add fade

--- js/TxtRotate.js
+++ js/TxtRotate.js
@@ -39,7 +39,9 @@ TxtRotate.prototype.tick = function() {
   }, delta);
 };
 
+
 window.onload = function() {
+  setTimeout(function () {
   var elements = document.getElementsByClassName('txt-rotate');
   for (var i=0; i<elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-rotate');
@@ -53,4 +55,4 @@ window.onload = function() {
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
-};
+},1000)};
